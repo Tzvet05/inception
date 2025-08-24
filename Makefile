9417d74f6abff1daa2fb1a28cd2436b6cf979c5d
ttrave <tzvetan.trave@abadcafe.org>
2025-08-24 21:18:02 +0200

    mariadb done, starting wordpress

--- Makefile
+++ Makefile
@@ -2,4 +2,12 @@
 
 all: up
 
-.PHONY: all start stop up down 
+up:
+	@ mkdir -p /home/ttrave/data/mariadb
+	@ mkdir -p /home/ttrave/data/wordpress
+	@ docker compose -f srcs/docker-compose.yml up --build -d
+
+down:
+	@ docker compose -f srcs/docker-compose.yml down -v
+
+.PHONY: all up down 
--- srcs/requirements/mariadb/mariadb_entrypoint.sh
+++ srcs/requirements/mariadb/mariadb_entrypoint.sh
@@ -1,8 +1,11 @@
+#!/bin/sh
+
 # Subsitute all environment variables in initialization file
 if [ -f /entrypoint-initdb.d/init.sql.template ]; then
+	mariadb-install-db --user=mysql --ldata=/var/lib/mysql
 	envsubst < /entrypoint-initdb.d/init.sql.template > /etc/mysql/init.sql
 	rm /entrypoint-initdb.d/init.sql.template
 fi
 
 # Start MariaDB
-exec "$@"
+$@
