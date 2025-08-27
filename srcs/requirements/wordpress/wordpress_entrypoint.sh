3488057669749cfa94d0fe4bce67e3fba1838d5b
ttrave <tzvetan.trave@abadcafe.org>
2025-08-27 08:08:20 +0200

    fixed broken wp & nginx

--- srcs/requirements/wordpress/wordpress_entrypoint.sh
+++ srcs/requirements/wordpress/wordpress_entrypoint.sh
@@ -1,21 +1,24 @@
 #!/bin/sh
 
-# Download WordPress files
-wp core download --allow-root
+# Increase PHP memory limit
+sed -i 's/^memory_limit\s*=.*/memory_limit = 512M/' /etc/php84/php.ini
 
-# Create config file
-if [ ! -f wp-config.php ]; then
-	wp config create --dbname=wordpress --dbuser=$MYSQL_USERNAME \
-		--dbpass=$MYSQL_PASSWORD --dbhost=mariadb --allow-root
+# Download WordPress files
+if [ ! -f index.php ]; then
+	wp core download --allow-root
 fi
 
-# Install WordPress
-wp core install --url=$DOMAIN_NAME --title=$TITLE \
-	--admin_user=$WORDPRESS_USERNAME_OP --admin_password=$WORDPRESS_PASSWORD_OP \
-	--admin_email=$WORDPRESS_EMAIL_OP --allow-root
-
-wp user create $WORDPRESS_USERNAME $WORDPRESS_EMAIL --role=author \
-	--user-pass=$WORDPRESS_PASSWORD --allow-root
+# Install WordPress, create config file and users
+if [ ! -f wp-config.php ]; then
+	wp core config --dbname=wordpress --dbuser=$MYSQL_USERNAME \
+		--dbpass=$MYSQL_PASSWORD --dbhost=mariadb --allow-root
+	wp core install --url=$DOMAIN_NAME --title=$TITLE \
+		--admin_user=$WORDPRESS_USERNAME_OP \
+		--admin_password=$WORDPRESS_PASSWORD_OP \
+		--admin_email=$WORDPRESS_EMAIL_OP --allow-root
+	wp user create $WORDPRESS_USERNAME $WORDPRESS_EMAIL --role=author \
+		--user-pass=$WORDPRESS_PASSWORD --allow-root
+fi
 
 # Start PHP
 exec "$@"
